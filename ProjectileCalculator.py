7432fb0cb9f513bd9563c110ca1e5e0bc1100d99
Cameron Carmichael Alonso <cameron@carmichaelalonso.co.uk>
2016-01-07 18:51:01 +0100

    Raw input for variables

--- ProjectileCalculator.py
+++ ProjectileCalculator.py
@@ -5,11 +5,36 @@ Copyright Cameron Carmichael Alonso, 2016. All Rights Reserved.
 
 """
 
+import argparse
+import sys
+
 ##
 ## Variables:
-## s - distance (x, y)
+## s - distance (x, y); R refers to s.x
 ## u - initial velocity (x, y; optional sin() or cos() component)
 ## v - velocity (x, y; optional sin() or cos() component)
 ## a - acceleration (x, y)
 ## t - time
 ##
+
+def calculateInitialVelocity():
+	print "Calculating initial velocity..."
+
+##
+## Entry point
+##
+if __name__ == "__main__":
+
+	print "\n\nEnter variables below. Keep blank if no value is to be specifed."
+
+	## get variables
+	s = raw_input("s (m) = ")
+	R = raw_input("R (m) = ")
+	u = raw_input("u (m/s) = ")
+	v = raw_input("v (m/s) = ")
+	a = raw_input("a (m/s^2) = ")
+	t = raw_input("t (s) = ")
+	theta = raw_input("Theta = ")
+	
+
+
