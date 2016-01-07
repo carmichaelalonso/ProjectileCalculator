eb445f89097a43557632ed4683f7ce92cc4fff4f
Cameron Carmichael Alonso <cameron@carmichaelalonso.co.uk>
2016-01-07 11:50:40 +0100

    First commit with venv and SymPy module

---
+++ ProjectileCalculator.py
@@ -0,0 +1,15 @@
+"""
+
+ProjectileCalculator.py
+Copyright Cameron Carmichael Alonso, 2016. All Rights Reserved.
+
+"""
+
+##
+## Variables:
+## s - distance (x, y)
+## u - initial velocity (x, y; optional sin() or cos() component)
+## v - velocity (x, y; optional sin() or cos() component)
+## a - acceleration (x, y)
+## t - time
+##
