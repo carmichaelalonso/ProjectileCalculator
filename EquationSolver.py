6736ddd2b50c0cdae03bc286d357267083ee4408
Cameron Carmichael Alonso <cameron@carmichaelalonso.co.uk>
2016-01-08 13:40:54 +0100

    Equation solvers for v=u+at and v^2=u^2+2as

--- EquationSolver.py
+++ EquationSolver.py
@@ -75,4 +75,150 @@ def sutat2(sVal, uVal, tVal, aVal):
 		result = equation[0]
 		print result
 
+		## blank line to keep tidy
+		print "\n\n"
+
+	return result
+
+def vuat(vVal, uVal, aVal, tVal):
+	## performs equation v = u + at
+	print "\nSolving using v = u + a t"
+
+	## define symbols for equation
+	v, u, a, t = symbols('v u a t')
+
+	##
+	## try and convert values to float
+	## when exception is found, keep string blank
+	##
+	try:
+		vFloat = float(vVal)
+	except:
+		vVal = ""
+
+	try:
+		uFloat = float(uVal)
+	except:
+		uVal = ""
+
+	try:
+		aFloat = float(aVal)
+	except:
+		aVal = ""
+
+	try:
+		tFloat = float(tVal)
+	except:
+		tVal = ""
+
+	##
+	## check which value is to be solved
+	##
+	if vVal is "":
+		## solve for v and substitute
+		print "Solving for v..."
+
+		equation = solve((-v+u+a*t).subs([(u, uFloat), (t, tFloat), (a, aFloat)]), v)
+		result = equation[0]
+		print result
+
+	elif uVal is "":
+		## solve for u and substitute
+		print "Solving for u..."
+
+		equation = solve((-v+u+a*t).subs([(t, tFloat), (v, vFloat), (a, aFloat)]), u)
+		result = equation[0]
+		print result
+
+	elif aVal is "":
+		## solve for a and substitute
+		print "Solving for a..."
+
+		equation = solve((-v+u+a*t).subs([(v, vFloat), (u, uFloat), (t, tFloat)]), a)
+		result = equation[0]
+		print result
+
+	elif tVal is "":
+		## solve for t and substitute
+		print "Solving for t..."
+
+		equation = solve((-v+u+a*t).subs([(v, vFloat), (u, uFloat), (a, aFloat)]), t)
+		result = equation[0]
+		print result
+
+		## blank line to keep tidy
+		print "\n\n"
+		
+	return result
+
+
+def vuas(vVal, uVal, aVal, sVal):
+	## performs equation v^2 = u^2 + 2as
+	print "\nSolving using v^2 = u^2 + 2as"
+
+	## define symbols for equation
+	v, u, a, s = symbols('v u a s')
+
+	##
+	## try and convert values to float
+	## when exception is found, keep string blank
+	##
+	try:
+		vFloat = float(vVal)
+	except:
+		vVal = ""
+
+	try:
+		uFloat = float(uVal)
+	except:
+		uVal = ""
+
+	try:
+		aFloat = float(aVal)
+	except:
+		aVal = ""
+
+	try:
+		sFloat = float(sVal)
+	except:
+		sVal = ""
+
+	##
+	## check which value is to be solved
+	##
+	if vVal is "":
+		## solve for v and substitute
+		print "Solving for v..."
+
+		equation = solve((-v**2+u**2+2*a*s).subs([(u, uFloat), (s, sFloat), (a, aFloat)]), v)
+		result = equation[0]
+		print result
+
+	elif uVal is "":
+		## solve for u and substitute
+		print "Solving for u..."
+
+		equation = solve((-v**2+u**2+2*a*s).subs([(v, vFloat), (a, aFloat), (s, sFloat)]), u)
+		result = equation[0]
+		print result
+
+	elif aVal is "":
+		## solve for a and substitute
+		print "Solving for a..."
+
+		equation = solve((-v**2+u**2+2*a*s).subs([(v, vFloat), (u, uFloat), (s, sFloat)]), a)
+		result = equation[0]
+		print result
+
+	elif sVal is "":
+		## solve for s and substitute
+		print "Solving for s..."
+
+		equation = solve((-v**2+u**2+2*a*s).subs([(v, vFloat), (u, uFloat), (a, aFloat)]), s)
+		result = equation[0]
+		print result
+
+		## blank line to keep tidy
+		print "\n\n"
+		
 	return result
